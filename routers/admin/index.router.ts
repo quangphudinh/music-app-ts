ade4260ac2737962d81591c2c3e99584e9f6e073
John Doe <johndoe@email.com>
2024-10-29 09:04:33 +0700

    trang chủ đề bài hát

--- routers/admin/index.router.ts
+++ routers/admin/index.router.ts
@@ -1,10 +1,14 @@
 import { Express } from 'express';
 import { dashboardRoutes } from './dashboard.router';
+import { topicRoutes } from './topic.router';
+
 import { systemConfig } from '../../config/config';
+
 
 const adminRoutes = (app : Express) : void => {
     const PATH_ADMIN  = `/${systemConfig.prefixAdmin}`;
     app.use(`${PATH_ADMIN}/dashboard` , dashboardRoutes);
+    app.use(`${PATH_ADMIN}/topics` , topicRoutes);
 }
 
 export default adminRoutes
